cd0568faa4d2ef49233f261e326b0c6d8c05c408
MZalfred <cutesy-todo0x@icloud.com>
2024-02-15 19:15:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const { Pool } = require('pg'); // Import Pool class from pg module
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
-
 const app = express();
 
-// Middleware for parsing JSON bodies
-app.use(express.json());
+// Middleware
+app.use(express.json()); // for parsing application/json
 
-// Use the auth routes
-app.use('/api/auth', authRoutes);
-
-// Create a new pool instance and pass in your PostgreSQL connection information
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // If using SSL connection (e.g., in production with Heroku), uncomment the following line
-  // ssl: { rejectUnauthorized: false }
+// Define a simple route for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// Test PostgreSQL connection
-pool.connect()
-  .then(() => console.log("PostgreSQL connected"))
-  .catch(err => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Set the application to listen on a specific port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
