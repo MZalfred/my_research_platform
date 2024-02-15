c2566b6003c77441d839c31f9dae5787cd597268
MZalfred <cutesy-todo0x@icloud.com>
2024-02-15 19:20:02 +0000

    mail

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define a route
-router.get('/api', (req, res) => {
-  res.json({ message: "This is your API endpoint" });
-});
-
-module.exports = router;
