d2bd1ba5dcf33f5fd8eb889864d44ab748c596d5
MZalfred <cutesy-todo0x@icloud.com>
2024-02-15 19:16:53 +0000

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { pool } = require('../db'); // Assume you've exported your pool instance from a db.js file
-
 const router = express.Router();
 
-// Register route
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 8);
-
-  pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id', [username, hashedPassword], (error, results) => {
-    if (error) {
-      return res.status(400).send(error);
-    }
-    res.status(201).send(`User added with ID: ${results.rows[0].id}`);
-  });
+// Define a route
+router.get('/api', (req, res) => {
+  res.json({ message: "This is your API endpoint" });
 });
 
-// Export the router
 module.exports = router;
