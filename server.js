20bf38c38bc737cafb522a08b5c5e1955c598a17
MZalfred <cutesy-todo0x@icloud.com>
2024-02-15 06:23:45 +0000

    mail

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+  
+
+app.use(express.json()); // Middleware for parsing JSON bodies
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
